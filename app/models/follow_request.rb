7c1d2250d76255d68dcb77a57123026a4b56f03b
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-01-01 23:36:35 +0300

    Add validation to FollowReuest model
    The validation ensures that a user is able to make only one follow
    to a particular user.

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -2,6 +2,8 @@ class FollowRequest < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
 
+  validates :follower_id, uniqueness: { scope: :following_id, message: "You have already sent a follow request to this user" }
+
   def approve_follow_request
     self.status = "approved"
     self.save
